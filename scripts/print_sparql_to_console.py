dd7f8e9212116bcd91a2d84c3fdea5f41e8a63e1
Raisha Abdillah <raisha.abdillah@gmail.com>
2016-09-08 12:00:10 +0300

    Update print_sparql_to_console.py

--- scripts/print_sparql_to_console.py
+++ scripts/print_sparql_to_console.py
@@ -6,8 +6,8 @@ This code will use pywikibot to generate the result from WikidataSPARQL Query
 and print it to the user's console.
 """
 
-with open('your_query.rq', 'r') as query_file: #replace your_query.rq with your own Wikidata SPARQL Query
-  QUERY = query_file.read().replace('\n', ' ')
+with open('your_query.rq', 'r') as query_file:  #replace your_query.rq with your own Wikidata SPARQL Query
+	QUERY = query_file.read().replace('\n', ' ')
 
 wikidata_site = pywikibot.Site("wikidata", "wikidata")
 generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
